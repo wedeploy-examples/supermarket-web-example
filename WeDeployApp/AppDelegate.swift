e02d7e96f83e3361b4ee78e3c219122007eb7843
Victor Galan <victor.galan@liferay.com>
2017-01-16 10:33:21 +0100

    Change main color everytime the app is launched

--- WeDeployApp/AppDelegate.swift
+++ WeDeployApp/AppDelegate.swift
@@ -12,11 +12,32 @@ import WeDeploy
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+	let mainColors = [
+		UIColor(8, 223, 133, 1),
+		UIColor(0, 164, 255, 1),
+		UIColor(149, 82, 239, 1),
+		UIColor(255, 64, 64, 1),
+		UIColor(255, 11, 110, 1),
+		UIColor(255, 183, 0, 1)
+	]
+
 	var window: UIWindow?
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
-		UIColor.mainColor = UIColor(149, 82, 239, 1)
+		let selectedColor = UserDefaults.standard.integer(forKey: "selectedColor")
+
+		let currentColor = selectedColor
+
+		UIColor.mainColor = mainColors[currentColor]
+
+		var nextColor = 0
+		if selectedColor != mainColors.count - 1 {
+			nextColor = currentColor + 1
+		}
+
+		UserDefaults.standard.set(nextColor, forKey: "selectedColor")
+
 
 		return true
 	}
