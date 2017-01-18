d5908c84b13a08dac0246ba6a8651aab3217ad82
Victor Galan <victor.galan@liferay.com>
2017-01-18 09:40:00 +0100

    Add data list screenlet

---
+++ WeDeployApp/Screenlets/DataListScreenlet.swift
@@ -0,0 +1,14 @@
+//
+//  DataListScreenleet.swift
+//  WeDeployApp
+//
+//  Created by Victor Galán on 16/01/2017.
+//  Copyright © 2017 liferay. All rights reserved.
+//
+
+import UIKit
+
+
+open class DataListScreenlet : BaseScreenlet {
+	public static let LoadDataAction = "LoadData"
+}
