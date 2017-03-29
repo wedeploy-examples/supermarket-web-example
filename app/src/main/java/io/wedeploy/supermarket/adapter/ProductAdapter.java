cdfb39068762771e5d3cf924689f77d499d3ae4e
Silvio Santos <silvio.santos@liferay.com>
2017-03-29 17:21:16 -0300

    Create product adapter

---
+++ app/src/main/java/io/wedeploy/supermarket/adapter/ProductAdapter.java
@@ -0,0 +1,56 @@
+package io.wedeploy.supermarket.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.wedeploy.supermarket.databinding.ItemProductBinding;
+import io.wedeploy.supermarket.model.Product;
+
+/**
+ * @author Silvio Santos
+ */
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder> {
+
+	@Override
+	public ProductViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+		LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+		ItemProductBinding binding = ItemProductBinding.inflate(inflater);
+
+		return new ProductViewHolder(binding);
+	}
+
+	@Override
+	public void onBindViewHolder(ProductViewHolder holder, int position) {
+		Product product = products.get(position);
+		holder.binding.setProduct(product);
+	}
+
+	@Override
+	public int getItemCount() {
+		return products.size();
+	}
+
+	public void setItems(List<Product> products) {
+		this.products.clear();
+		this.products.addAll(products);
+		notifyDataSetChanged();
+	}
+
+	class ProductViewHolder extends RecyclerView.ViewHolder {
+
+		ItemProductBinding binding;
+
+		public ProductViewHolder(ItemProductBinding binding) {
+			super(binding.getRoot());
+
+			this.binding = binding;
+		}
+	}
+
+	private List<Product> products = new ArrayList<>();
+
+}
