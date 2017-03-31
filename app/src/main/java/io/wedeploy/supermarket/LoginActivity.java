6cbfc299f4e43603be44d0b1fe509821f60d37ba
Silvio Santos <silvio.santos@liferay.com>
2017-03-31 16:15:11 -0300

    Login Activity layout

---
+++ app/src/main/java/io/wedeploy/supermarket/LoginActivity.java
@@ -0,0 +1,18 @@
+package io.wedeploy.supermarket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author Silvio Santos
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_login);
+    }
+
+}
