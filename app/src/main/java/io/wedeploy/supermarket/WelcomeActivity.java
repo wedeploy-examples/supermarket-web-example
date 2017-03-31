bf0f24f5e336ad7807f07990d63f306c11f0fc23
Silvio Santos <silvio.santos@liferay.com>
2017-03-31 20:56:20 -0300

    Set app icon on Welcome screen

--- app/src/main/java/io/wedeploy/supermarket/WelcomeActivity.java
+++ app/src/main/java/io/wedeploy/supermarket/WelcomeActivity.java
@@ -1,7 +1,12 @@
 package io.wedeploy.supermarket;
 
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import io.wedeploy.supermarket.databinding.ActivityWelcomeBinding;
 
 /**
  * @author Silvio Santos
@@ -12,6 +17,15 @@ public class WelcomeActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_welcome);
+        ActivityWelcomeBinding binding = DataBindingUtil.setContentView(
+                this, R.layout.activity_welcome);
+
+        binding.signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
+            }
+        });
+
     }
 }
