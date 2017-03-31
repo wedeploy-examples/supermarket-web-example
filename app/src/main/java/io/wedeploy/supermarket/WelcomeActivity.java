0ca84abb333bc42208f9520e52b87edffa42f842
Silvio Santos <silvio.santos@liferay.com>
2017-03-31 10:20:01 -0300

    Create WelcomeActivity

---
+++ app/src/main/java/io/wedeploy/supermarket/WelcomeActivity.java
@@ -0,0 +1,17 @@
+package io.wedeploy.supermarket;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author Silvio Santos
+ */
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_welcome);
+    }
+}
